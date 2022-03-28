b432b1eca96320d1a2d9c86fb4138664c5526392
KevinJonathan117 <kevinjonathan3010@gmail.com>
2022-03-28 14:00:07 +0700

    Initial Commit

---
+++ MovieDirectory/MovieDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDirectoryApp.swift
+//  MovieDirectory
+//
+//  Created by Kevin Jonathan on 28/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
