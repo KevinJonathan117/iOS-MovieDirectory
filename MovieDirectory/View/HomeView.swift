2e04976a0f1cace1eaf375e5974d6961116d88cb
KevinJonathan117 <kevinjonathan3010@gmail.com>
2022-03-30 00:52:06 +0700

    Changed the list of movies to movies with poster (scroll horizontally)

--- MovieDirectory/View/HomeView.swift
+++ MovieDirectory/View/HomeView.swift
@@ -16,15 +16,56 @@ struct HomeView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                List(viewModel.movies) { movie in
-                    NavigationLink {
-                        DetailView()
-                    } label: {
-                        Text(movie.title)
+            VStack(alignment: .leading) {
+                Text("Popular")
+                    .fontWeight(.bold)
+                    .padding()
+                    .font(.title2)
+                
+                ScrollView (.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(viewModel.movies) { movie in
+                            VStack {
+                                AsyncImage(
+                                    url: URL(string: "https://image.tmdb.org/t/p/w500/\(movie.posterPath)"),
+                                    content: { image in
+                                        image.resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: 120, height: 160)
+                                    },
+                                    placeholder: {
+                                        Spacer()
+                                        
+                                        ProgressView()
+                                            .frame(width: 120, height: 160)
+                                        
+                                        Spacer()
+                                    }
+                                )
+                                
+                                Spacer()
+                                
+                                NavigationLink {
+                                    DetailView()
+                                } label: {
+                                    Text(movie.title)
+                                        .multilineTextAlignment(.leading)
+                                        .foregroundColor(.primary)
+                                }
+                                .frame(width: 120)
+                                
+                                Spacer()
+                            }
+                            .padding(.trailing)
+                        }
+                        
                     }
                 }
                 .onAppear(perform: viewModel.getPopularMovies)
+                .frame(height: 250)
+                .padding(.leading)
+                
+                Spacer()
             }
             .navigationTitle("Movie List")
             .navigationBarItems(trailing: NavigationLink {
